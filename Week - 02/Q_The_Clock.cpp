766ade4f92c471562c458681d78a05cc1ddb69cf
Md Shahrul Islam Sayem <129561775+muhammad-sayem@users.noreply.github.com>
2023-10-12 20:22:04 +0600

    Add files via upload

--- Week - 02/Q_The_Clock.cpp
+++ Week - 02/Q_The_Clock.cpp
@@ -33,25 +33,43 @@ int main()
         string s;
         cin >> s;
 
-        int n;
-        cin >> n;
+        int x;
+        cin >> x;
 
-        string str1, str2;
+        int h = x/60;
+        int m = x%60;
 
-        str1 += s[0];
-        str1 += s[1];
+        int ans = 0;
 
-        str2 += s[3];
-        str2 += s[4];
+        map<string, bool> visited;
 
-        int val1 = stoi(str1);
-        int val2 = stoi(str2);
+        while(!visited[s]){
+            if(isPalindrome(s)) ans++;
+            visited[s] = true;
 
-        cout << val1 << " " << val2 << endl;
+            int hh = ((s[0]-'0')*10) + (s[1]-'0');
+            int mm = ((s[3]-'0')*10) + (s[4]-'0');
+
+            mm += m;
+
+            if(mm > 59){
+                hh++;
+            }
+
+            mm = mm%60;
+
+            hh += h;
+            hh = hh%24;
+
+            s[0] = ('0' + (hh/10));
+            s[1] = ('0' + (hh%10));
+
+            s[3] = ('0' + (mm/10));
+            s[4] = ('0' + (mm%10));
+
+        }
+        cout << ans << endl;
     }
-
-    
-
 
     return 0;
 }
