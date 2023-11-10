942e74db0297969dff79f2b6acca57dddfeed078
Md Shahrul Islam Sayem <129561775+muhammad-sayem@users.noreply.github.com>
2023-11-10 21:50:03 +0600

    Add files via upload

---
+++ Week - 07/G_Codechef_Favourite_Numbers.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+#define      endl            '\n'
+#define      yes             (cout << "YES\n")
+#define      no              (cout << "NO\n")
+#define      ll              long long
+#define      pb              push_back
+#define      pii             pair <int,int>
+#define      pll             pair <long long,long long>
+#define      rev(a)          reverse(a, a+n);
+#define      Srev(s)         reverse(s.begin(), s.end());
+#define      sz(s)           s.size()
+#define      gcd(a,b)        __gcd(a,b)
+#define      lcm(a,b)        (a*b)/gcd(a,b)
+#define      fastIO          ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+int main()
+{
+    fastIO;
+
+    int test;
+    cin >> test;
+
+    while(test--){
+        int n;
+        cin >> n;
+
+        if(!(n&1) && (n%7) == 0) cout << "Alice" << endl;
+        else if((n&1) && (n%9) == 0) cout << "Bob" << endl;
+        else cout << "Charlie" << endl;
+    }
+
+    return 0;
+}
