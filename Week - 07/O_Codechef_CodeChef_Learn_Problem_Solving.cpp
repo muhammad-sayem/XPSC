1c30e3bab50837d567aebc328d1b61b6fe3e285c
Md Shahrul Islam Sayem <129561775+muhammad-sayem@users.noreply.github.com>
2023-11-12 22:01:39 +0600

    Add files via upload

---
+++ Week - 07/O_Codechef_CodeChef_Learn_Problem_Solving.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#define      nl              '\n'
+#define      yes             (cout << "YES\n")
+#define      no              (cout << "NO\n")
+#define      yy              (cout << "Yes\n")
+#define      nn              (cout << "No\n")
+#define      ll              long long
+#define      pb              push_back
+#define      pii             pair <int,int>
+#define      pll             pair <long long,long long>
+#define      rev(a)          reverse(a, a+n);
+#define      Srev(s)         reverse(s.begin(), s.end());
+#define      sz(s)           s.size()
+#define      gcd(a,b)        __gcd(a,b)
+#define      lcm(a,b)        (a*b)/gcd(a,b)
+#define      fastIO          ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+int main()
+{
+    fastIO;
+
+    int n; 
+    cin >> n; 
+    
+    cout << 2*n << nl;
+    
+    return 0;
+}
---
+++ Week - 07/R_Codechef_Four_Tickets.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+#define      endl            '\n'
+#define      yes             (cout << "YES\n")
+#define      no              (cout << "NO\n")
+#define      ll              long long
+#define      pb              push_back
+#define      pii             pair <int,int>
+#define      pll             pair <long long,long long>
+#define      rev(a)          reverse(a, a+n);
+#define      Srev(s)         reverse(s.begin(), s.end());
+#define      sz(s)           s.size()
+#define      gcd(a,b)        __gcd(a,b)
+#define      lcm(a,b)        (a*b)/gcd(a,b)
+#define      fastIO          ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+int main()
+{
+    fastIO;
+
+    int test;
+    cin >> test;
+
+    while(test--){
+        ll n;
+        cin >> n;
+
+        if((n*4) <= 1000) yes;
+        else no;
+    }
+
+    return 0;
+}
