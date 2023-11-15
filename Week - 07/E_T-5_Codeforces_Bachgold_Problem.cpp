112a47939f36733f71d45e2559abe56301839381
Md Shahrul Islam Sayem <129561775+muhammad-sayem@users.noreply.github.com>
2023-11-15 23:26:52 +0600

    Add files via upload

---
+++ Week - 07/E_T-5_Codeforces_Bachgold_Problem.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+#define      endl            '\n'
+#define      yes             (cout << "YES\n")
+#define      no              (cout << "NO\n")
+#define      ll              long long
+#define      pb              push_back
+#define      pii             pair <int,int>
+#define      pll             pair <long long,long long>
+#define      rev(a)          reverse(a, a+n);
+#define      Srev(s)         reverse(s.begin(), s.end());
+#define      sz(s)           s.size()
+#define      gcd(a,b)        __gcd(a,b)
+#define      lcm(a,b)        (a*b)/gcd(a,b)
+#define      fastIO          ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+int main()
+{
+    fastIO;
+
+    int n, i;
+    cin >> n;
+
+    cout << n/2 << endl;
+
+    if(n%2 == 0){
+        for(i=1; i<=n/2; i++){
+            cout << 2 << " ";
+        }
+        cout << endl;
+    }
+
+    else{
+        for(i=1; i<n/2; i++){
+            cout << 2 << " ";
+        }
+        cout << 3 << endl;
+    }
+
+    return 0;
+}
