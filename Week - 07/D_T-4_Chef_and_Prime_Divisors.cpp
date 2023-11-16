af9f709234faccfd699f865769d68fb739801e90
Md Shahrul Islam Sayem <129561775+muhammad-sayem@users.noreply.github.com>
2023-11-16 23:37:45 +0600

    Add files via upload

---
+++ Week - 07/D_T-4_Chef_and_Prime_Divisors.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+#define      nl              '\n'
+#define      yes             (cout << "YES\n")
+#define      no              (cout << "NO\n")
+#define      yy              (cout << "Yes\n")
+#define      nn              (cout << "No\n")
+#define      ll              long long
+#define      pb              push_back
+#define      pii             pair <int,int>
+#define      pll             pair <long long,long long>
+#define      rev(a)          reverse(a, a+n);
+#define      Srev(s)         reverse(s.begin(), s.end());
+#define      sz(s)           s.size()
+#define      gcd(a,b)        __gcd(a,b)
+#define      lcm(a,b)        (a*b)/gcd(a,b)
+#define      fastIO          ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+int main()
+{
+    fastIO;
+
+    int test;
+    cin >> test;
+
+    while(test--){
+        ll a, b;
+        cin >> a >> b;
+
+        while(1){
+            ll GCD = gcd(a, b);
+
+            if(GCD == 1){
+                break;
+            }
+            b /= GCD;
+        }
+
+        if(b == 1) yy;
+        else nn;
+    }
+
+    return 0;
+}
