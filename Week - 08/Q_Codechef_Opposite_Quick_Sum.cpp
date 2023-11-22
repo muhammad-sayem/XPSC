258d1331c2ce78f8bf248574a14b5d1096a6aeaf
Md Shahrul Islam Sayem <129561775+muhammad-sayem@users.noreply.github.com>
2023-11-22 23:21:54 +0600

    Add files via upload

---
+++ Week - 08/Q_Codechef_Opposite_Quick_Sum.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+#define      nl              '\n'
+#define      yes             (cout << "YES\n")
+#define      no              (cout << "NO\n")
+#define      yy              (cout << "Yes\n")
+#define      nn              (cout << "No\n")
+#define      ll              long long
+#define      pb              push_back
+#define      pii             pair <int,int>
+#define      pll             pair <long long,long long>
+#define      rev(a)          reverse(a, a+n);
+#define      Srev(s)         reverse(s.begin(), s.end());
+#define      sz(s)           s.size()
+#define      gcd(a,b)        __gcd(a,b)
+#define      lcm(a,b)        (a*b)/gcd(a,b)
+#define      fastIO          ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+int main()
+{
+    fastIO;
+
+    int test;
+    cin >> test;
+
+    while(test--){
+        int n;
+        cin >> n;
+
+        int i = 1, j = n;
+
+        while(i<j){
+            cout << i << " " << j << " ";
+            i++;
+            j--;
+        }
+        cout << nl;
+    }
+
+    return 0;
+}
