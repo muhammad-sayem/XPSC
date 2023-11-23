b785640959f918f6b6d47923a2d3461cc008dc74
Md Shahrul Islam Sayem <129561775+muhammad-sayem@users.noreply.github.com>
2023-11-23 21:11:52 +0600

    Add files via upload

---
+++ Week - 08/V_Codechef_Equal_Elements.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+#define      nl              '\n'
+#define      yes             (cout << "YES\n")
+#define      no              (cout << "NO\n")
+#define      yy              (cout << "Yes\n")
+#define      nn              (cout << "No\n")
+#define      ll              long long
+#define      pb              push_back
+#define      pii             pair <int,int>
+#define      pll             pair <long long,long long>
+#define      rev(a)          reverse(a, a+n);
+#define      Srev(s)         reverse(s.begin(), s.end());
+#define      sz(s)           s.size()
+#define      gcd(a,b)        __gcd(a,b)
+#define      lcm(a,b)        (a*b)/gcd(a,b)
+#define      fastIO          ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+int main()
+{
+    fastIO;
+
+    int test;
+    cin >> test;
+
+    while(test--){
+        int n, i;
+        cin >> n;
+        int a[n+5];
+        int fre[200005] = {0};
+
+        for(i=0; i<n; i++){
+            cin >> a[i];
+            fre[a[i]]++;
+        }
+        
+        int maxx = *max_element(fre, fre+n);
+
+        cout << n - maxx << nl;
+    }
+
+    return 0;
+}
